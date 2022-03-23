a49d61fec1a3ed0c4c3d5fc9b076af72a18e573d
Landen Doty <landot2002@gmail.com>
2022-03-22 19:14:01 -0500

    transition from landing page to main menu

--- src/static/visualizer.js
+++ src/static/visualizer.js
@@ -1,3 +1,8 @@
 function dropdown_buttons() {
   document.getElementById("dropdown-menu").classList.toggle("show");
 }
+
+function move_to_main() {
+  document.getElementById("landing-page").style.display = "none";
+  document.getElementById("visualizer-navbar").style.display = "block";
+}
