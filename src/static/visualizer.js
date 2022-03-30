1ddf2c80c6a916430067d058e8c0669bc32c14c2
ezhuo56 <ericzhuo56@gmail.com>
2022-03-30 14:47:04 -0500

    Updated stackTable

--- src/static/visualizer.js
+++ src/static/visualizer.js
@@ -35,13 +35,6 @@ function data_info(data_structure) {
     ' Pop removes the element at the top of the stack. Peek allows the user to see the value of the top element');
   }
 } 
-function Shape(x, y, w, h, fill) {
-  this.x = x;
-  this.y = y;
-  this.w = w;
-  this.h = h;
-  this.fill = fill;
-}
 var elem = document.getElementById('canvas');
 
 function stackTable()
@@ -49,21 +42,19 @@ function stackTable()
   if (data_structure==1)
   {
     if (elem.getContext) {
-
-      var myRect = [];
-    
-      myRect.push(new Shape(0, 0, 25, 25, "#333"));
-      myRect.push(new Shape(0, 40, 39, 25, "#333"));
-      myRect.push(new Shape(0, 80, 100, 25, "#333"));
-    
-      context = elem.getContext('2d');
-      for (var i in myRect) {
-          oRec = myRect[i];
-          context.fillStyle = oRec.fill;
-          context.fillRect(oRec.x, oRec.y, oRec.w, oRec.h);
-    
+         context = elem.getContext('2d');
+         context.beginPath();
+         context.rect(400, 150, 100, 100);
+         context.rect(500, 150, 100, 100);
+         context.rect(600, 150, 100, 100);
+         context.rect(700, 150, 100, 100);
+         context.rect(800, 150, 100, 100);
+         context.rect(900, 150, 100, 100);
+         context.rect(1000, 150, 100, 100);
+         context.rect(1100, 150, 100, 100);
+         context.rect(1200, 150, 100, 100);
+         context.rect(1300, 150, 100, 100);
+         context.stroke();
       }
     }
   }
-}
-
