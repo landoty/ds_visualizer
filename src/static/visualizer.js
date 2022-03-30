d48865ce1b8c176e0ab9a973889a3583e40a64f1
Duy Vu <d053v574@ku.edu>
2022-03-30 18:44:57 -0500

    styled the stack and added title for stack page

--- src/static/visualizer.js
+++ src/static/visualizer.js
@@ -1,7 +1,7 @@
 var data_structure = 0; //Stack = 1, Queue = 2, etc..
-var top_X = 400;
-const top_Y = 150;
-var stack = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var top_X = 320;
+const top_Y = 350;
+var stack = [];
 var top_index = stack.length-1;
 
 function dropdown_buttons() {
@@ -30,6 +30,7 @@ function moveToStack() {
   document.getElementById("stack-page").style.display = "block";
   data_structure = 1;
   stackTable();
+  startStackTable();
 }
 
 function data_info(data_structure) {
@@ -48,11 +49,15 @@ function stackTable()
     if (elem.getContext) {
         context = elem.getContext("2d");
         context.beginPath();
-        top_X = 400;
+        top_X = 320;
         for (let i=0; i<stack.length; i++) {
-          context.rect(top_X, top_Y, 100, 100);
-          top_X+=100;
+          context.rect(top_X, top_Y, 130, 200);
+          context.fillStyle = "#FAF9F6";
+          context.fillRect(top_X, top_Y, 130, 200);
+          top_X+=130;
         }
+        context.lineWidth = "4";
+        context.strokeStyle = "black";
         context.stroke();
       }
     }
@@ -60,7 +65,16 @@ function stackTable()
 
 function stack_push(value){
   if(stack.length < 10){
-    let new_stack_value = prompt("Please enter a value to push onto the stack:")
+    let new_stack_value = prompt("Please enter a value to push onto the stack:");
+    if(new_stack_value=="")
+    {
+      alert("You cannot enter a empty string!")
+      return;
+    }
+    if(new_stack_value==null)
+    {
+      return;
+    }
     context = elem.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
     stack.push(new_stack_value);
@@ -68,7 +82,7 @@ function stack_push(value){
     stackTable();
   }
   else{
-    alert("Stack length is limited to only 10 elements for this program!")
+    alert("Stack length is limited to only 10 elements for this program!");
   }
 }
 
@@ -91,5 +105,17 @@ function stack_peek(){
   }
   else{
     alert("Peek can not be performed on an empty stack!");
+  }
+  for (let i=0; i<stack.length; i++){
+    let value = stack[i];
+    console.log(value);
+  }
+}
+
+function startStackTable()
+{
+  for(let i=0; i<10;i++)
+  {
+    context.clearRect(0, 0, canvas.width, canvas.height);
   }
 }
