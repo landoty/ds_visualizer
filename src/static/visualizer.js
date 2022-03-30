9dbf80b9ebc896d0da40549e7438c7a141b78703
ezhuo56 <ericzhuo56@gmail.com>
2022-03-30 14:30:10 -0500

    Rectangle Table Prototype

--- src/static/visualizer.js
+++ src/static/visualizer.js
@@ -25,6 +25,7 @@ window.onclick = function(event) {
 function moveToStack() {
   document.getElementById("stack-page").style.display = "block";
   data_structure = 1;
+  stackTable();
 }
 
 function data_info(data_structure) {
@@ -34,4 +35,35 @@ function data_info(data_structure) {
     ' Pop removes the element at the top of the stack. Peek allows the user to see the value of the top element');
   }
 } 
+function Shape(x, y, w, h, fill) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.fill = fill;
+}
+var elem = document.getElementById('canvas');
+
+function stackTable()
+{
+  if (data_structure==1)
+  {
+    if (elem.getContext) {
+
+      var myRect = [];
+    
+      myRect.push(new Shape(0, 0, 25, 25, "#333"));
+      myRect.push(new Shape(0, 40, 39, 25, "#333"));
+      myRect.push(new Shape(0, 80, 100, 25, "#333"));
+    
+      context = elem.getContext('2d');
+      for (var i in myRect) {
+          oRec = myRect[i];
+          context.fillStyle = oRec.fill;
+          context.fillRect(oRec.x, oRec.y, oRec.w, oRec.h);
+    
+      }
+    }
+  }
+}
 
