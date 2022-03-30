1a351346942a3f9ecdaa5c661284ae064fedec30
Duy Vu <d053v574@ku.edu>
2022-03-30 16:41:01 -0500

    implemented pop/peek and started a bit on push

--- src/static/visualizer.js
+++ src/static/visualizer.js
@@ -1,4 +1,8 @@
 var data_structure = 0; //Stack = 1, Queue = 2, etc..
+var top_X = 400;
+const top_Y = 150;
+var stack = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var top_index = stack.length-1;
 
 function dropdown_buttons() {
   document.getElementById("dropdown-menu").classList.toggle("show");
@@ -25,6 +29,7 @@ window.onclick = function(event) {
 function moveToStack() {
   document.getElementById("stack-page").style.display = "block";
   data_structure = 1;
+  stackTable();
 }
 
 function data_info(data_structure) {
@@ -34,4 +39,54 @@ function data_info(data_structure) {
     ' Pop removes the element at the top of the stack. Peek allows the user to see the value of the top element');
   }
 } 
+var elem = document.getElementById('canvas');
 
+function stackTable()
+{
+  if (data_structure==1)
+  {
+    if (elem.getContext) {
+        context = elem.getContext("2d");
+        context.beginPath();
+        top_X = 400;
+        for (let i=0; i<stack.length; i++) {
+          context.rect(top_X, top_Y, 100, 100);
+          top_X+=100;
+        }
+        context.stroke();
+      }
+    }
+  }
+
+function stack_push(value){
+  if(stack.length < 10){
+    let new_stack_value = prompt("Please enter a value to push onto the stack:")
+    stack.push(new_stack_value);
+    top_index++;
+  }
+  else{
+    alert("Stack length is limited to only 10 elements for this program!")
+  }
+}
+
+function stack_pop(){
+  if(stack.length > 0){
+    context = elem.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    stack.pop();
+    top_index--;
+    stackTable();
+  }
+  else{
+    alert("You can not perform a pop on an empty stack!");
+  }
+}
+
+function stack_peek(){
+  if(stack.length != 0){
+    alert("The value in the top element is " + stack[top_index] + "!");
+  }
+  else{
+    alert("Peek can not be performed on an empty stack!");
+  }
+}
